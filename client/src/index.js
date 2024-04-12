fa76919d2de96330f350d3cf89c2157c9d996e15
MathewJenik <62413639+MathewJenik@users.noreply.github.com>
2024-04-12 21:50:18 +1000

    react frontend changes for production

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import { store } from './app/Store';
 
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
