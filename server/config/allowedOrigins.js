c6e222b097927db13ae22a48c3c3085034d6741c
MathewJenik <mathewjenik1@gmail.com>
2024-04-24 16:50:14 +1000

    updates

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
     'http://apps.mathewjenik.com:3000',
+    'http://apps.mathewjenik.com:3500',
     'http://apps.mathewjenik.com',
     'https://mathewjenik.com'
 ]
